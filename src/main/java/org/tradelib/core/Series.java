f5863478097d009649003b88e455bc26b523a8a8
Ivan Popivanov <ivan.popivanov@gmail.com>
2015-08-10 18:44:32 -0400

    Changed the license header.

--- src/main/java/org/tradelib/core/Series.java
+++ src/main/java/org/tradelib/core/Series.java
@@ -1,4 +1,4 @@
-// Copyright 2015 by Ivan Popivanov
+// Copyright 2015 Ivan Popivanov
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
